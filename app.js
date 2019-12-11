fe798900f2df7f9c0a4aa24937c873fcadd944ac
Ismael Arias <ismaeljarias@gmail.com>
2019-12-11 14:17:20 -0400

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
 
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
 // Conexion a la base de datos
-mongoose.connect('mongodb://localhost:27017/hospitalDB', { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
+mongoose.connect('mongodb://localhost:27017/hospitalDB', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true }, (err, res) => {
   if(err) throw err;
   console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 });
 
 
 // Rutas
-app.get('/', (req, res, next) => {
-  res.status(200).json({
-    ok: true,
-    mensaje: 'Peticion realizada correctamente'
-  });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 // Escuchar peticiones
 app.listen(3000, () => {
---
+++ models/usuario.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var Schema = mongoose.Schema;
+
+var rolesValidos = {
+  values: ['ADMIN_ROLE', 'USER_ROLE'],
+  message: '{VALUE} no es un rol permitido'
+};
+
+var usuarioSchema = new Schema({
+  nombre: { type: String, required: [true, 'Nombre es necesario'] },
+  email: { type: String, unique:true, required: [true, 'El correo es necesario'] },
+  password: { type: String, required: [true, 'Password es necesario'] },
+  img: { type: String, required: false },
+  role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos }
+});
+
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser unico' });
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
